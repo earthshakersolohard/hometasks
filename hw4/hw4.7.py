4784eff93b8e311d2d99b3a3b540544e94694a5b
earthshakersolohard <1st.bianconeri@gmail.com>
2022-04-18 12:46:29 +0600

    hw4

---
+++ hw4/hw4.7.py
@@ -0,0 +1,26 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+from functools import reduce
+from itertools import count
+def fact(n):
+    f = 1
+    for i in range(2, n + 1):
+        f *= i
+        yield f
+
+for el in fact(7):
+    print(el)
